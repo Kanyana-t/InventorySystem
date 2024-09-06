005f2a4fe44511a43bf008bc2bcdcfecdd727b51
KANYANAT SUDCHOEICHOM <kanyanat.sudc@bu.ac.th>
2024-09-06 18:10:14 +0700

    Complete Project

---
+++ Pages/Siam/EditSiam.cshtml.cs
@@ -0,0 +1,93 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+
+namespace InventorySystem.Pages.Siam
+{
+    public class EditSiamModel : PageModel
+    {
+        public StockInfo stockInfo = new StockInfo();
+        public String errorMessage = "";
+        public String successMessage = "";
+
+        public void OnGet()
+        {
+            String itemid = Request.Query["itemid"];
+
+            try
+            {
+                String connectionString = "Server=tcp:pakaowbu.database.windows.net,1433;Initial Catalog=InventoryPakaow;Persist Security Info=False;User ID=pakaowbu;Password=!Bu123456;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "select * from stocks where itemid = @itemid;";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@itemid", itemid);
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                stockInfo.itemid = "" + reader.GetInt32(0);
+                                stockInfo.item = reader.GetString(1);
+                                stockInfo.storeid = reader.GetString(2);
+                                stockInfo.supplier = reader.GetString(3);
+                                stockInfo.amount = reader.GetString(4);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMessage = "Exception: " + ex.ToString();
+            }
+
+        }
+
+        public void OnPost()
+        {
+            stockInfo.itemid = Request.Form["itemid"];
+            stockInfo.item = Request.Form["item"];
+            stockInfo.storeid = Request.Form["storeid"];
+            stockInfo.supplier = Request.Form["supplier"];
+            stockInfo.amount = Request.Form["amount"];
+
+            if (stockInfo.item.Length == 0 || stockInfo.storeid.Length == 0 ||
+                stockInfo.supplier.Length == 0 || stockInfo.amount.Length == 0)
+            {
+                errorMessage = "Please fill all fields";
+                return;
+            }
+            try
+            {
+                String connectionString = "Server=tcp:inventoryy.database.windows.net,1433;Initial Catalog=Inventory;Persist Security Info=False;User ID=kanyanat;Password=Inventory15;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "UPDATE stocks " +
+                                 "SET item=@item, storeid=@storeid, supplier=@supplier, amount=@amount " +
+                                 "WHERE itemid=@itemid;";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@itemid", stockInfo.itemid);
+                        command.Parameters.AddWithValue("@item", stockInfo.item);
+                        command.Parameters.AddWithValue("@storeid", stockInfo.storeid);
+                        command.Parameters.AddWithValue("@supplier", stockInfo.supplier);
+                        command.Parameters.AddWithValue("@amount", stockInfo.amount);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+                return;
+            }
+
+            Response.Redirect("/Siam/IndexSiam");
+
+        }
+    }
+}
